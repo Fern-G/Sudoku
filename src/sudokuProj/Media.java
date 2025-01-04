f72a9799756153789bd93d901d45e0ae096cc7b3
FernJG <fernandog@vt.edu>
2025-01-03 23:39:57 -0500

    more frame work

--- src/sudokuProj/Media.java
+++ src/sudokuProj/Media.java
@@ -1,5 +1,8 @@
 package sudokuProj;
 
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+
 import javax.swing.*;
 
 public class Media {
@@ -7,7 +10,21 @@ public class Media {
 	//private JFrame window;
 	
 	public Media() {
-		JFrame window = new JFrame();
-		JPanel win = new JPanel();
+		JFrame frame = new JFrame(); // creates the window
+		JButton button = new JButton("grid1");
+		
+		JPanel panel = new JPanel(); //creates panel to add stuff
+		panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));// border of panel
+		panel.setLayout(new GridLayout(0, 1));
+		panel.add(button);
+		
+		frame.add(panel, BorderLayout.CENTER);//adding the panel
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);//closing window
+		frame.setTitle("Sudoku");
+		frame.pack();// for window to match size
+		frame.setVisible(true);
+		
+		ImageIcon image = new ImageIcon("icon.png");
+		frame.setIconImage(image.getImage());
 	}
 }
